604118fd2a5cbaa399c6a42108fee714e238d444
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-09-03 15:57:53 -0300

    Colocando a opçao de retorna ao menu principal

--- src/br/com/zup/Desafio.java
+++ src/br/com/zup/Desafio.java
@@ -10,37 +10,41 @@ public class Desafio {
         Map<String, String> funcionarios = new HashMap<String, String>();
 
         int menu = 0;
+        int escolhaMenu = 1;
 
 
         System.out.println("Olá, bem vindo!");
         System.out.println("Irei te ajudar a gerenciar uma lista de funcionários.");
-        System.out.println("Digite 1: Para adicionar funcionário");
-        System.out.println("Digite 2: Para remover funcionário");
-        System.out.println("Digite 3: Para sair do menu");
-        menu = leitor.nextInt();
-        leitor.nextLine();
 
-        switch (menu) {
-            case 1:
-                System.out.println("Por favor, digite o nome do funcionário: ");
-                String nome = leitor.nextLine();
-                System.out.println("Por favor, digite o número do funcionário: ");
-                String telefone = leitor.nextLine();
-                System.out.println("Por favor, digite o CPF do funcionário: ");
-                String cpf = leitor.nextLine();
-                System.out.println("Por favor, digite o email do funcionário: ");
-                String email = leitor.nextLine();
+        while (escolhaMenu == 1){
+            System.out.println("Digite 1: Para adicionar funcionário");
+            System.out.println("Digite 2: Para remover funcionário");
+            menu = leitor.nextInt();
+            leitor.nextLine();
+            switch (menu) {
+                case 1:
+                    System.out.println("Por favor, digite o nome do funcionário: ");
+                    String nome = leitor.nextLine();
+                    System.out.println("Por favor, digite o número do funcionário: ");
+                    String telefone = leitor.nextLine();
+                    System.out.println("Por favor, digite o CPF do funcionário: ");
+                    String cpf = leitor.nextLine();
+                    System.out.println("Por favor, digite o email do funcionário: ");
+                    String email = leitor.nextLine();
 
-                funcionarios.put(cpf, "Nome: " + nome + " Telefone: " + telefone + " Email: " + email);
-                System.out.println(funcionarios);
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            default:
-                System.out.println("Número invalido.");
-                break;
+                    funcionarios.put(cpf, "Nome: " + nome + " Telefone: " + telefone + " Email: " + email);
+                    System.out.println(funcionarios);
+                    break;
+                case 2:
+                    break;
+                default:
+                    System.out.println("Número invalido.");
+                    break;
+            }
+            System.out.println("Digite 1: Para voltar ao menu principal");
+            escolhaMenu = leitor.nextInt();
         }
+        System.out.println("Programa finalizado");
+
     }
 }
