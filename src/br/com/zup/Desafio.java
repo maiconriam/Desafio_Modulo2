19566311dc05b8887491d7225a490ddee6c2f1aa
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-09-03 15:50:02 -0300

    Colocando um menu usando switch

--- src/br/com/zup/Desafio.java
+++ src/br/com/zup/Desafio.java
@@ -9,18 +9,38 @@ public class Desafio {
         Scanner leitor = new Scanner(System.in);
         Map<String, String> funcionarios = new HashMap<String, String>();
 
+        int menu = 0;
+
+
         System.out.println("Olá, bem vindo!");
         System.out.println("Irei te ajudar a gerenciar uma lista de funcionários.");
-        System.out.println("Por favor, digite o nome do funcionário: ");
-        String nome = leitor.nextLine();
-        System.out.println("Por favor, digite o número do funcionário: ");
-        String telefone = leitor.nextLine();
-        System.out.println("Por favor, digite o CPF do funcionário: ");
-        String cpf = leitor.nextLine();
-        System.out.println("Por favor, digite o email do funcionário: ");
-        String email = leitor.nextLine();
+        System.out.println("Digite 1: Para adicionar funcionário");
+        System.out.println("Digite 2: Para remover funcionário");
+        System.out.println("Digite 3: Para sair do menu");
+        menu = leitor.nextInt();
+        leitor.nextLine();
 
-        funcionarios.put(cpf, "Nome: " + nome + " Telefone: " + telefone + " Email: " + email);
-        System.out.println(funcionarios);
+        switch (menu) {
+            case 1:
+                System.out.println("Por favor, digite o nome do funcionário: ");
+                String nome = leitor.nextLine();
+                System.out.println("Por favor, digite o número do funcionário: ");
+                String telefone = leitor.nextLine();
+                System.out.println("Por favor, digite o CPF do funcionário: ");
+                String cpf = leitor.nextLine();
+                System.out.println("Por favor, digite o email do funcionário: ");
+                String email = leitor.nextLine();
+
+                funcionarios.put(cpf, "Nome: " + nome + " Telefone: " + telefone + " Email: " + email);
+                System.out.println(funcionarios);
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+                System.out.println("Número invalido.");
+                break;
+        }
     }
 }
